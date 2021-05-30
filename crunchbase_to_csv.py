53822c8f142d1ad0238c5af4a2adb74547fa873d
Andrew Schell <11813209+datatalking@users.noreply.github.com>
2021-05-30 09:48:48 -0700

    Initial commit - script to pull data from cruchbase, database practice and training data for future scraping

---
+++ crunchbase_to_csv.py
@@ -0,0 +1,36 @@
+# SOURCE https://rapidapi.com/blog/crunchbase-api-python-automated-sales-pipeline/
+# FILENAME cruchbase_to_csv.py
+
+# Press ⇧F10 to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# Press the green button in the gutter to run the script.
+def main():
+    cruchbase_data_grab():
+
+
+def functions_inside_cruchbase_to_csv():
+    print(cruchbase_data_grab.__doc__)
+    print(print_hi().__doc__)
+
+
+def cruchbase_data_grab():
+    """A rest api from rapidAPI to extract json from cruchbase datacenter"""
+
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    main()
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
